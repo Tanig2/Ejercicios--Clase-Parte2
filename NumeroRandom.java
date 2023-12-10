a77b7b7059bcfcd37d1fcf8919409e73b6e3e73f
Tania Gomez Garcia <taniag2g@gmail.com>
2023-12-10 18:44:43 +0100

    Acabo de añadir mi primer programa al repositoriogit status

---
+++ NumeroRandom.java
@@ -0,0 +1,21 @@
+import java.util.Random;
+
+public class NumeroRandom {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Vamos a crear el numero random
+		
+		Random rd = new Random();
+		
+		int numRandom;
+		numRandom = rd.nextInt(15); //entre 0 y 15
+		System.out.printf("%f\n",numRandom);
+		
+	//estoy añadiendo un comentario para modificar el programa para GitHub	
+		
+
+	}
+
+}
